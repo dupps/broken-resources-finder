9378a1486cb126ffe2f68bbaaa8921068ea9a14f
Dupps, Bastian <dupps@email.de>
2017-07-29 12:31:00 +0200

    add badge for broken resources count
    * send message on popup activation

---
+++ background_scripts/resource_checker.js
@@ -0,0 +1,77 @@
+'use strict';
+
+let brokenResources = [];
+let brokenCount = new Map();
+let activeTab;
+
+function logURL(requestDetails) {
+
+    if (requestDetails.statusCode >= 400) {
+
+        let url = requestDetails.url;
+        let reason = requestDetails.statusLine;
+        let tabId = requestDetails.tabId;
+
+        handleBrokenResource(url, reason, tabId);
+    }
+}
+
+function logError(requestDetails) {
+
+    let url = requestDetails.url;
+    let reason = requestDetails.error;
+    let tabId = requestDetails.tabId;
+
+    handleBrokenResource(url, reason, tabId)
+}
+
+function handleBrokenResource(url, reason, tabId) {
+
+    console.debug("Loading: " + url);
+    console.debug("Reason: " + reason);
+    console.debug("TabId: " + tabId);
+
+    if (brokenCount.get(tabId)) {
+        brokenCount.set(tabId, brokenCount.get(tabId) + 1)
+    } else {
+        brokenCount.set(tabId, 1)
+    }
+
+    browser.browserAction.setBadgeText({text: (brokenCount.get(tabId)).toString(), tabId: tabId});
+
+    brokenResources.push({
+        url: url,
+        reason: reason
+    });
+}
+
+function handleMessage(request, sender, sendResponse) {
+    console.log("Message from the content script: " +
+        request.greeting);
+    sendResponse({response: "Response from background script"});
+}
+
+function handleActivated(activeInfo) {
+
+    console.log("Tab " + activeInfo.tabId + " was activated");
+    activeTab = activeInfo.tabId;
+}
+
+function handleUpdated(tabId, changeInfo, tabInfo) {
+
+    brokenCount.delete(tabId);
+}
+
+browser.webRequest.onCompleted.addListener(
+    logURL,
+    {urls: ["<all_urls>"], types: ["image", "stylesheet", "script"]}
+);
+
+browser.webRequest.onErrorOccurred.addListener(
+    logError,
+    {urls: ["<all_urls>"], types: ["image", "stylesheet", "script"]}
+);
+
+browser.runtime.onMessage.addListener(handleMessage);
+browser.tabs.onActivated.addListener(handleActivated);
+browser.tabs.onUpdated.addListener(handleUpdated);
--- popup/js/popup.js
+++ popup/js/popup.js
@@ -1,3 +1,20 @@
 'use strict';
 
 // todo: options anchor
+
+function notifyPopup() {
+    let sending = browser.runtime.sendMessage({
+        greeting: "Greeting from the content script"
+    });
+    sending.then(handleResponse, handleError);
+}
+
+function handleResponse(message) {
+    console.log(`Message from the popup script:  ${message.response}`);
+}
+
+function handleError(error) {
+    console.log(`Error: ${error}`);
+}
+
+notifyPopup();
